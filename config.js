1b41b43590fdf6dd84b27bfe36daad4851c89cb5
sudrajatlatief9 <sudrajatlatief9@gmail.com>
2026-06-28 17:01:41 +0700

    edit config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.RW26_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzp7I29gi1gvK9Ry0s-3nzZFXFNbYxB5EbqMP07g6zzCWGbk99gmSUZwOP3mHpggryy/exec",
-  PUBLIC_ACTION: "publicContent"
+  PUBLIC_ACTION: "publicContent_"
 };
